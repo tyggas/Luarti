ed44eff681bde556849fbbc378727cc60e303743
Tiago Grigull <tagrigull@gmail.com>
2024-09-30 10:50:34 -0300

    Add files via upload

--- src/java/utils/Conexao.java
+++ src/java/utils/Conexao.java
@@ -1,13 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package utils;
 
-/**
- *
- * @author tiago_grigull
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexao {
-    
+	public static Connection conectar() throws ClassNotFoundException {
+		Connection con = null;
+		String url = "jdbc:mysql://localhost:3306/luarti";
+		String user = "root";
+		String password = "";
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection(url, user, password);
+		} catch (SQLException ex)
+
+		{
+			System.out.println("Erro ao conectar com o banco " + ex);
+
+		}
+		return con;
+
+	}
 }
